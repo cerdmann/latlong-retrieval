b18999d9bbb54271eb866fc1f1a7d37d518d38e9
Erds <erds@badmoondev.com>
2017-09-14 15:01:17 -0500

    Adding dependencies and code to register with service registry

--- src/main/java/com/example/latlongretrieval/LatlongRetrievalApplication.java
+++ src/main/java/com/example/latlongretrieval/LatlongRetrievalApplication.java
@@ -2,8 +2,10 @@ package com.example.latlongretrieval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LatlongRetrievalApplication {
 
 	public static void main(String[] args) {
