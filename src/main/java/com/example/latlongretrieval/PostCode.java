53d9772907a1afaff5c8e358a7b4103e4163f929
cerdmann <craigerdmann@gmail.com>
2026-03-05 19:42:36 -0600

    removing unused imports

--- src/main/java/com/example/latlongretrieval/PostCode.java
+++ src/main/java/com/example/latlongretrieval/PostCode.java
@@ -2,13 +2,12 @@ package com.example.latlongretrieval;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.example.latlongretrieval.Place;
 import java.util.*;
 import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-public class PostCode{
+public class PostCode {
 
   @JsonProperty(value = "post code")
   private String postCode;
