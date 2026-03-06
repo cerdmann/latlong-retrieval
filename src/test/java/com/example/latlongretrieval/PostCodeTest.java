3eca28d8733b799f21924bafad4d9920386e0f73
cerdmann <craigerdmann@gmail.com>
2026-03-05 20:10:53 -0600

    changed spacing

--- src/test/java/com/example/latlongretrieval/PostCodeTest.java
+++ src/test/java/com/example/latlongretrieval/PostCodeTest.java
@@ -1,9 +1,7 @@
 package com.example.latlongretrieval;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PostCodeTest {
